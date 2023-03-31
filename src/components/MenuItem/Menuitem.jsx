e16a26753375be60f09636da0ddcbc3c91defec1
ibro001 <ibrahimmoses56@gmail.com>
2023-03-31 12:28:33 +0100

    completed the Menuitem component

--- src/components/MenuItem/Menuitem.jsx
+++ src/components/MenuItem/Menuitem.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Menuitem.css';
 
-const Menuitem = () => {
+const Menuitem = ({ title, price, tags }) => {
   return (
-    <div>Menuitem</div>
+    <div className='app__menuitem'>
+      <div className='app__menuitem-head'>
+        <div className='app__menuitem-name'>
+          <div className='p__cormorant' style={{color: '#DCCA87'}}>
+            {title}
+          </div>
+        </div>
+
+        <div className='app__menuitem-dash' />
+
+        <div className='app__menuitem-price'>
+          <div className='p__cormorant'>
+            {price}
+          </div>
+        </div>
+      </div>
+      <div className='app__menuitem-sub'>
+        <p className='p__opensans' style={{color: '#AAA'}}>{tags}</p>
+      </div>
+    </div>
   )
 }
 
