527f627d3311fe12d036a32222c31665eecb995c
ibro001 <ibrahimmoses56@gmail.com>
2023-03-31 12:24:07 +0100

    completed the header container

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Header.css';
+import { images } from '../../constants'
+import SubHeading from '../../components/subHeading/SubHeading';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__wrapper section__padding'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Chase The New Flavour' />
+        <h1 className='app__header-h1'>
+          The Key to Fine Dining
+        </h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>
+          Sit tellus lobortis sed senectus vivamus molestie. 
+          Condimentum volutpat morbi facilisis quam scelerisque sapien.
+          Et, penatibus aliquam amet tellus 
+        </p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.welcome} alt="header_img" />
+      </div>
+    </div>
   )
 }
 
