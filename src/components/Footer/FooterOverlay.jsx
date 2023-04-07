c1cc64d7c22b9676214edb289759252ac3a01520
ibro001 <ibrahimmoses56@gmail.com>
2023-04-07 21:43:43 +0100

    completed the footer overlay component

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,7 +3,10 @@ import './FooterOverlay.css';
 
 const FooterOverlay = () => {
   return (
-    <div>Footer</div>
+    <div className='app__footerOverlay'>
+      <div  className='app__footerOverlay-black'/>
+      <div  className='app__footerOverlay-img app__bg'/>
+    </div>
   )
 }
 
